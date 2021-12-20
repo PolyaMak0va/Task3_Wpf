708b1f903cc2043a82ae6fd4ebc79d0920397080
Ксения Финаева <polya.makova@outlook.com>
2021-12-20 20:08:06 +0300

    Добавьте файлы проекта.

---
+++ Task3_Wpf/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example1_WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+
+            int newValue;
+            if (int.TryParse(fontSize, out newValue))
+            {
+                if (textBox != null)
+                {
+                    textBox.FontSize = newValue;
+                }
+            }
+        }
+        private void btnBold_Click(object sender, RoutedEventArgs e)
+        {
+            //int weightValue = 999;
+            //textBox.FontWeight = FontWeight.FromOpenTypeWeight(weightValue);
+            if (textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+        private void btnItalic_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+        private void btnUnderline_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == TextDecorations.Underline)
+            {
+                textBox.TextDecorations = null;
+            }
+            else
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+        }
+        private void rb1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rb1.IsChecked == true)
+            {
+                if (textBox != null)
+                {
+                    textBox.Foreground = Brushes.Black;
+                }
+            }
+            else if (rb2.IsChecked == true)
+            {
+                if (textBox != null)
+                {
+                    textBox.Foreground = Brushes.Red;
+                }
+            }
+        }
+    }
+}
